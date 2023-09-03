70c9a39cde47f619c4af32bfd6373039dba1ddd3
“沈轻腾“ <sqt18750016193@163.com>
2023-09-03 16:09:02 +0800

    fix minSDK

--- inject/build.gradle.kts
+++ inject/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        minSdk = 30
+        minSdk = 27
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")

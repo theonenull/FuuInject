054f71526b376b6b4a560efeb3b184ffe6fba01c
“沈轻腾“ <sqt18750016193@163.com>
2023-09-03 14:49:18 +0800

    fix gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,11 +43,7 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
     }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
+
 }
 
 dependencies {
